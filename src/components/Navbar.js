0dee22195666493ef08188b7adfde9b01649a095
Sajal Tiwari <sajaltiwari1007@gmail.com>
2024-08-22 15:13:00 +0530

    did some changes in nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar({mode,setmode}) {
        {/* <Router>
        <Routes>
         <Route> */}
-               <Link className='pl-2 font-large   ' to="/"  > Dsa tracker </Link>
+               <Link className='pl-2 font-large   ' to="/"  > Code Challenger </Link>
        {/* </Route>
        </Routes>
        </Router> */}
